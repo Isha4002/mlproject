a2254f72e143c7defc93ce59eb970845c82695a7
Isha Pal <ishapal2004@gmail.com>
2026-06-11 22:45:04 +0530

    Project deployed successfully on Render

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,redirect, url_for
 import numpy as np
 import pandas as pd
 
@@ -13,7 +13,7 @@ app=application
 
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    return redirect(url_for('predict_datapoint'))
 
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
@@ -26,8 +26,8 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+            reading_score=float(request.form.get('reading_score')),
+            writing_score=float(request.form.get('writing_score'))
 
         )
         pred_df=data.get_data_as_data_frame()
